0758b9ad9ad35fa0141d6be7c20e5e321f10f602
Pavel Kuzmin <Virus191288@gmail.com>
2023-08-11 00:15:32 +0500

    metrics code refactor more prompts

--- prompt.md
+++ prompt.md
@@ -1,18 +1,47 @@
-You are an assistant designed to help developers migrate their code from Vue 2 to Vue 3 using Typescript with Composition API. Here is a set of rules you must absolutely follow:
-1. Rewrite the <script lang="ts"> to <script setup lang="ts">
-2. The content of the script tag must be valid Typescript code
-3. The component must be flattened into the script setup
-4. Remove any "export default".
-5. Use the `onMounted` hook instead of the `created` lifecycle hook if necessary
-6. Use the `useRoute` approach instead of $route. Same for $router.
-7. Store is not using vuex but pinia.
-8. Auth-related functions are accessible in stores/auth.ts, using useAuthStore.
-9. Do not use Ref if the type can be inferred from the value passed into ref()
-10. Do not put all the methods and properties into a global const object
-11. Prefer using global "const router = useRouter()" instead of live instantiation when needed
+# Task
 
-example: 
-vue 2:
+You are an AI assistant designed to help developers migrate their code from Vue 2 to Vue 3 using TypeScript with the Composition API. Your task is to refactor code following the rules outlined below:
+
+## Rules
+
+1. Rewrite `<script lang="ts">` to `<script setup lang="ts">`.
+2. The content of the script tag must be valid TypeScript code.
+3. The component must be flattened into the script setup.
+4. Remove any "export default".
+5. Use the `onMounted` hook instead of the `created` lifecycle hook if necessary.
+6. Use the `useRoute` and `useRouter` approach instead of `$route`. Same for `$router`.
+7. Store is not using Vuex but Pinia.
+8. Auth-related functions are accessible in stores/auth.ts, using useAuthStore.
+9. Do not use Ref if the type can be inferred from the value passed into ref().
+10. Do not put all the methods and properties into a global const object.
+11. Prefer using global "const router = useRouter()" instead of live instantiation when needed.
+12. Replace any usage of `this.$router` or `this.$route` with the `router` or `route` from Vue Router 4's `useRouter` and `useRoute`.
+13. Replace any usage of `this.localePath` with `localePath` from Nuxt's `useLocalePath`.
+14. Replace any usage of `this.globalStore` with `globalStore` from Pinia's `useStore`.
+15. Replace any watchers with Vue 3's `watch` function.
+16. Replace any usage of `this.$i18n` or `this.$i18n.t` with `i18n.t` with `i18n` from Vue I18n's `useI18n`.
+17. Use `useHead` from `vueuse/head` to manage the document head.
+18. Replace any usage of `this.$nuxtI18nHead` with `useLocaleHead` from Nuxt's `vue-i18n`.
+19. Replace any usage of `this.$nuxtI18nHead` with `head` from Nuxt's `useHead`.
+20. Replace any usage of `this.localePath` with `localePath` from Nuxt's `useLocalePath`.
+21. Replace any usage of `this.$api` with `api` from your own `useApi`.
+22. Replace any usage of `this.$store` with the corresponding store from Pinia's `useStore`.
+23. Replace any usage of `asyncData` with `onMounted` and async/await.
+24. Replace any usage of `mounted` with `onMounted`.
+25. Replace any usage of `this.$route.query` with `route.query`.
+26. Replace any usage of `this.$route.params` with `route.params`.
+27. Replace any usage of `useJsonld` with the `jsonld` method in the `@Jsonld` decorator.
+28. Replace any usage of `ref` with class property and `this.propertyName`.
+29. Convert any functions defined in the `script` section to class methods.
+30. Replace any usage of `this.$router.push` with `router.push`.
+
+These rules cover most of the changes in your provided examples. However, you may need to add more rules depending on the specificities of your project.
+
+## Example
+
+Here's an example of code in Vue 2 and its corresponding refactoring in Vue 3:
+
+### Vue 2
 ```vue
 <script lang="ts">
 import useGlobalStore from '@/store/global';
@@ -61,7 +90,7 @@ export default class IndexProxy extends Vue {
 }
 </script>
 ```
-result vue 3
+### Vue 3
 ```vue
 <script setup lang="ts">
 import { computed, onMounted, watch } from 'vue';
--- src/index.ts
+++ src/index.ts
@@ -1,147 +1,192 @@
 import fs from 'fs';
 import { Configuration, OpenAIApi } from 'openai';
-import { program } from 'commander';
+import { Command } from 'commander';
 import glob from 'glob';
+import ProgressBar from 'progress';
+import Table from 'cli-table3';
 import {getPrompt, getToken, wait} from "./helpers";
 
 const { version } = require('../package.json')
 
 const RE_SCRIPT = /(<script lang="ts">.*<\/script>)/s;
 
-async function refactorFilesInDirectory(
-    directory: string,
-    model: string,
-    token: string,
-    prompt: string,
-    timeout = 20000,
-    max_tokens = 4096
-): Promise<void> {
-    const files = glob.sync(`${directory}/**/*.vue`);
+class RefactorEngine {
+    private bar: ProgressBar;
+    private metrics: { file: string, old_length: number, new_length: number, time: number }[];
+    private model: string;
+    private token: string;
+    private prompt: string;
+    private timeout: number;
+    private max_tokens: number;
 
-    for (const file of files) {
-        console.log(`converting file: ${file}`);
-        try {
-            await refactor(file, model, token, prompt, max_tokens);
-        } catch (error) {
-            console.error(`An error occurred while refactoring file: ${file}`);
-            console.error(error);
-        }
-        await wait(timeout); // Ожидание перед отправкой следующего запроса
+    constructor(model: string, token: string, prompt: string, timeout = 20000, max_tokens = 4096) {
+        this.model = model;
+        this.token = token;
+        this.prompt = prompt;
+        this.timeout = timeout;
+        this.max_tokens = max_tokens;
+        this.bar = new ProgressBar(':bar :percent :etas', { total: 100 });
+        this.metrics = [];
     }
-}
 
-async function refactor(
-    filename: string,
-    model: string,
-    token: string,
-    prompt: string,
-    max_tokens = 4096
-): Promise<void> {
-    const content = fs.readFileSync(filename, 'utf8');
+    async refactorFilesInDirectory(directory: string): Promise<void> {
+        const files = glob.sync(`${directory}/**/*.vue`);
+        this.bar.total = files.length;
 
-    // Extract the script tag from <script lang="ts"> to </script>
-    const match = content.match(RE_SCRIPT);
-    if (!match) {
-        console.log('ERR: No script tag found');
-        process.exit(1);
-    }
-    const [scriptTag] = match;
-    const spanstart = match.index;
-    if (spanstart === undefined) {
-        console.log('ERR: Unable to determine script tag index');
-        process.exit(1);
-    }
-    const spanend = spanstart + scriptTag.length;
-
-    // Ask for refactoring
-    const configuration = new Configuration({
-        apiKey: token,
-    });
-
-    const openaiInstance = new OpenAIApi(configuration);
-    const response = await openaiInstance.createChatCompletion({
-        model,
-        max_tokens,
-        messages: [
-            { role: 'system', content: prompt },
-            { role: 'user', content: scriptTag },
-        ],
-    });
-
-    // Get the refactored script
-    let refactoredContent = content.slice(0, spanstart);
-    for (const choice of response.data.choices) {
-        if (choice.finish_reason !== 'stop') break;
-
-        const message = choice.message?.content;
-        if (message) {
-            refactoredContent += message;
-        }
-    }
-    refactoredContent += content.slice(spanend);
-
-    // Save the refactored content to a new file
-    const newFilename = filename.replace('.vue', '_refactored.vue');
-    fs.writeFileSync(newFilename, refactoredContent, 'utf8');
-
-    console.log(`Refactored code saved to ${newFilename}`);
-}
-
-async function main(): Promise<void> {
-    program
-        .name('vue3-migrate')
-        .description('CLI utilizes ChatGPT to automatically refactor Vue.js code from version 2 to version 3')
-        .version(version);
-
-    program
-        .command('directory')
-        .arguments('<directory>')
-        .description('Refactor all Vue files in a directory')
-        .option('-m, --model <model>', 'Specify the GPT model', 'gpt-3.5-turbo-16k')
-        .option('-t, --token <token>', 'Specify the GPT token', '')
-        .option('-p, --prompt <prompt>', 'Start prompt path', '')
-        .option('-t, --timeout <timeout>', 'Specify the timeout in milliseconds (default: 20000)', parseInt)
-        .option('-mt, --max_tokens <max_tokens>', 'GPT max tokens (default: 4096)', parseInt)
-        .action(async (directory, options) => {
-            const model = options.model;
-            const token = getToken(options);
-            const prompt = getPrompt(options);
-            const timeout = options.timeout || 20000;
-            const max_tokens = options.max_tokens || 4096;
-
-            await refactorFilesInDirectory(directory, model, token, prompt, timeout, max_tokens);
-        });
-
-    program
-        .command('convert')
-        .arguments('<filename>')
-        .description('Refactor Vue file in a directory')
-        .option('-m, --model <model>', 'Specify the GPT model', 'gpt-3.5-turbo-16k')
-        .option('-t, --token <token>', 'Specify the GPT token', '')
-        .option('-p, --prompt <prompt>', 'Start prompt path', '')
-        .option('-mt, --max_tokens <max_tokens>', 'GPT max tokens (default: 4096)', parseInt)
-        .action(async (filename, options) => {
-            if (!filename) {
-                console.log('ERR: No file specified');
-                process.exit(1);
+        for (const file of files) {
+            console.log(`converting file: ${file}`);
+            try {
+                await this.refactor(file);
+            } catch (error) {
+                console.error(`An error occurred while refactoring file: ${file}`);
+                console.error(error);
             }
-            const model = options.model;
-            const token = getToken(options);
-            const prompt = getPrompt(options);
-            const max_tokens = options.max_tokens || 4096;
+            this.bar.tick();
+            await wait(this.timeout); // Ожидание перед отправкой следующего запроса
+        }
+    }
 
-            await refactor(filename, model, token, prompt, max_tokens);
+    async refactor(filename: string): Promise<void> {
+        const start = Date.now();
+        const content = fs.readFileSync(filename, 'utf8');
+
+        // Extract the script tag from <script lang="ts"> to </script>
+        const match = content.match(RE_SCRIPT);
+        if (!match) {
+            console.log('ERR: No script tag found');
+            process.exit(1);
+        }
+        const [scriptTag] = match;
+        const spanstart = match.index;
+        if (spanstart === undefined) {
+            console.log('ERR: Unable to determine script tag index');
+            process.exit(1);
+        }
+        const spanend = spanstart + scriptTag.length;
+
+        const old_len = scriptTag.length;
+
+        // Ask for refactoring
+        const configuration = new Configuration({
+            apiKey: this.token,
         });
 
-    program.parse(process.argv);
+        const openaiInstance = new OpenAIApi(configuration);
+        const response = await openaiInstance.createChatCompletion({
+            model: this.model,
+            max_tokens: this.max_tokens,
+            messages: [
+                { role: 'system', content: this.prompt },
+                { role: 'user', content: scriptTag },
+            ],
+        });
+
+        // Get the refactored script
+        let refactoredContent = content.slice(0, spanstart);
+        let length = 0;
+        for (const choice of response.data.choices) {
+            if (choice.finish_reason !== 'stop') break;
+
+            const message = choice.message?.content;
+            if (message) {
+                refactoredContent += message;
+                length += message.length;
+            }
+        }
+        refactoredContent += content.slice(spanend);
+
+        // Save the refactored content to a new file
+        const newFilename = filename.replace('.vue', '_refactored.vue');
+        fs.writeFileSync(newFilename, refactoredContent, 'utf8');
+
+        console.log(`Refactored code saved to ${newFilename}`);
+
+
+        const time = Date.now() - start;
+        this.metrics.push({ file: filename, old_length: old_len, new_length: length, time });
+    }
+
+    printMetrics(): void {
+        const table = new Table({
+            head: ['File', 'Old Length', 'New Length', 'Time (ms)'],
+            colWidths: [100, 10, 10, 20]
+        });
+
+
+        for (const metric of this.metrics) {
+            table.push([metric.file, metric.old_length, metric.new_length, `${metric.time}ms`]);
+        }
+
+        console.log(table.toString());
+    }
 }
 
-export default main;
+class ProgramRunner {
+    private program: Command;
+
+    constructor() {
+        this.program = new Command();
+        this.program
+            .name('vue3-migrate')
+            .description('CLI utilizes ChatGPT to automatically refactor Vue.js code from version 2 to version 3')
+            .version(version);
+    }
+
+    run(): void {
+        this.program
+            .command('directory')
+            .arguments('<directory>')
+            .description('Refactor all Vue files in a directory')
+            .option('-m, --model <model>', 'Specify the GPT model', 'gpt-3.5-turbo-16k')
+            .option('-t, --token <token>', 'Specify the GPT token', '')
+            .option('-p, --prompt <prompt>', 'Start prompt path', '')
+            .option('-t, --timeout <timeout>', 'Specify the timeout in milliseconds (default: 20000)', parseInt)
+            .option('-mt, --max_tokens <max_tokens>', 'GPT max tokens (default: 4096)', parseInt)
+            .action(async (directory, options) => {
+                const model = options.model;
+                const token = getToken(options);
+                const prompt = getPrompt(options);
+                const timeout = options.timeout || 20000;
+                const max_tokens = options.max_tokens || 4096;
+
+                const engine = new RefactorEngine(model, token, prompt, timeout, max_tokens);
+                await engine.refactorFilesInDirectory(directory);
+
+                engine.printMetrics();
+            });
+
+        this.program
+            .command('convert')
+            .arguments('<filename>')
+            .description('Refactor Vue file in a directory')
+            .option('-m, --model <model>', 'Specify the GPT model', 'gpt-3.5-turbo-16k')
+            .option('-t, --token <token>', 'Specify the GPT token', '')
+            .option('-p, --prompt <prompt>', 'Start prompt path', '')
+            .option('-mt, --max_tokens <max_tokens>', 'GPT max tokens (default: 4096)', parseInt)
+            .action(async (filename, options) => {
+                if (!filename) {
+                    console.log('ERR: No file specified');
+                    process.exit(1);
+                }
+                const model = options.model;
+                const token = getToken(options);
+                const prompt = getPrompt(options);
+                const timeout = options.timeout || 20000;
+                const max_tokens = options.max_tokens || 4096;
+
+                const engine = new RefactorEngine(model, token, prompt, timeout, max_tokens);
+                await engine.refactor(filename);
+
+                engine.printMetrics();
+            });
+
+        this.program.parse(process.argv);
+    }
+}
+
+export default ProgramRunner;
 
 // Run the main function if the file is executed directly
 if (require.main === module) {
-    main().catch((error) => {
-        console.error('An error occurred:', error);
-        process.exit(1);
-    });
+    new ProgramRunner().run();
 }
